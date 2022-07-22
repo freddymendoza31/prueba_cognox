9b4c7b26e23f6731dd83210741e945f133a11d51
freddy mendoza <freddymendoza.padilla@gmail.com>
2022-07-22 11:19:43 -0500

    prueba

--- app/Http/Controllers/TransferirController.php
+++ app/Http/Controllers/TransferirController.php
@@ -12,7 +12,6 @@ class TransferirController extends Controller
 {
     public function transferir(Request $request)
     {
-        die($request->input('id_destino'));
         $insert = new TransferirModel();
         $insert->cuenta_origen = $request->input('cuenta_origen');
         $insert->cuenta_destino = $request->input('cuenta_destino');
